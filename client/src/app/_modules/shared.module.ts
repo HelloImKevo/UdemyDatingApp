f6d3c922a4b41daf1b734d4d63e3ecafcb9d0651
Kevin Schanz <kevinschanz@live.com>
2023-04-14 18:59:20 -0400

    Tidy up the app module by using a shared module

---
+++ client/src/app/_modules/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+
+/**
+ * Created with command:
+ * ```
+ * ng g module _modules/shared --flat
+ * ```
+ */
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
