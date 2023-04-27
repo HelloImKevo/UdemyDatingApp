f370a353767407cf420f60587da0b8535289354e
Kevin Schanz <kevinschanz@live.com>
2023-04-27 17:11:04 -0400

    Improve display of bulleted validation errors

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -38,7 +38,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                   }
                 }
 
-                throw modelStateErrors;
+                throw modelStateErrors.flat();
               } else {
                 this.toastr.error(error.error, error.status.toString());
               }
