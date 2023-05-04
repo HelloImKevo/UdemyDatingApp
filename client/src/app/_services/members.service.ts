175b944136c9d9cab74ff11109e1b1cd6d8af7f1
Kevin Schanz <kevinschanz@live.com>
2023-05-04 18:25:03 -0400

    Use an interceptor to send the JWT Bearer token

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
-import { User } from '../_models/user';
 
 /**
  * Created with command:
@@ -19,23 +18,10 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions())
+    return this.http.get<Member[]>(this.baseUrl + 'users')
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
-  }
-
-  getHttpOptions() {
-    const userString = localStorage.getItem('user');
-
-    if (!userString) return;
-
-    const user: User = JSON.parse(userString);
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + user.token
-      })
-    }
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
