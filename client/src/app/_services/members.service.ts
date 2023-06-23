8c3b3a83da269f225cb3e0c6a18eef7ac2a812e2
Kevin Schanz <kevinschanz@live.com>
2023-06-23 18:09:08 -0400

    Using the Members Service to store state during navigation between pages

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
-import { Observable } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 
 /**
  * Created with command:
@@ -15,18 +15,39 @@ import { Observable } from 'rxjs';
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users')
+    if (this.members.length > 0) return of(this.members);
+
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.userName === username);
+    if (member) return of(member);
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member): Observable<any> {
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        // The spread (...) syntax allows an iterable, such as an array or string,
+        // to be expanded in places where zero or more arguments (for function calls)
+        // or elements (for array literals) are expected. In an object literal, the
+        // spread syntax enumerates the properties of an object and adds the key-value
+        // pairs to the object being created.
+        this.members[index] = { ...this.members[index], ...member }
+      })
+    );
   }
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -14,7 +15,7 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members: Member[] = [];
+  members$: Observable<Member[]> | undefined;
 
   constructor(private memberService: MembersService) { }
 
@@ -23,8 +24,6 @@ export class MemberListComponent implements OnInit {
   }
 
   loadMembers(): void {
-    this.memberService.getMembers().subscribe({
-      next: members => this.members = members
-    })
+    this.members$ = this.memberService.getMembers();
   }
 }
