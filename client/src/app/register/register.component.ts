9a284ca509ae033c165e62ee523136bd1e003466
Kevin Schanz <kevinschanz@live.com>
2023-07-26 18:06:48 -0400

    Add initial Reactive Forms for Member Registration

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 /**
  * Created with command:
@@ -16,23 +17,51 @@ import { ToastrService } from 'ngx-toastr';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8)
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchValues('password')
+      ])
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : { notMatching: true }
+    }
   }
 
   registerNewUser() {
-    this.accountService.registerApi(this.model).subscribe({
-      next: () => {
-        // Close the 'Register' form.
-        this.cancel();
-      },
-      error: error => {
-        this.toastr.error(error.error);
-        console.log(error);
-      }
-    });
+    console.log(this.registerForm?.value);
+
+    // this.accountService.registerApi(this.model).subscribe({
+    //   next: () => {
+    //     // Close the 'Register' form.
+    //     this.cancel();
+    //   },
+    //   error: error => {
+    //     this.toastr.error(error.error);
+    //     console.log(error);
+    //   }
+    // });
   }
 
   cancel() {
