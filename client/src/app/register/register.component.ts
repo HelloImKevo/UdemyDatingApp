dce5fd8720e08b43ab29f43bda50ed7c03e641f0
Kevin Schanz <kevinschanz@live.com>
2023-07-27 17:37:50 -0400

    Light refactor to replace new FormGroup instance with FormBuilder service

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 /**
  * Created with command:
@@ -19,24 +19,29 @@ export class RegisterComponent implements OnInit {
   model: any = {}
   registerForm: FormGroup = new FormGroup({});
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.initializeForm();
   }
 
   initializeForm(): void {
-    this.registerForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      password: new FormControl('', [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(8)
-      ]),
-      confirmPassword: new FormControl('', [
-        Validators.required,
-        this.matchValues('password')
-      ])
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(8)
+        ]
+      ],
+      confirmPassword: ['',
+        [
+          Validators.required,
+          this.matchValues('password')
+        ]
+      ]
     });
     this.registerForm.controls['password'].valueChanges.subscribe({
       next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
