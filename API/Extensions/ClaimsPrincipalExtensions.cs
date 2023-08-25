768aa4d65f1cba97f5f6c03902f0d5c1773a17fa
Kevin Schanz <kevinschanz@live.com>
2023-08-25 18:23:55 -0400

    Optimize the Last Active action filter with small tweak to JWT Identifier

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -6,7 +6,14 @@ namespace API.Extensions
     {
         public static string GetUsername(this ClaimsPrincipal user)
         {
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            // Maps to: JwtRegisteredClaimNames.UniqueName
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            // Maps to: JwtRegisteredClaimNames.NameId
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
         }
     }
 }
