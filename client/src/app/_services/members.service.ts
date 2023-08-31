9adac7fa15bc1b647e7b905c69fd9956f9bcba4d
Kevin Schanz <kevinschanz@live.com>
2023-08-31 17:27:07 -0400

    Restore caching for member 'Detailed' view using reduce() function

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -42,6 +42,49 @@ export class MembersService {
     );
   }
 
+  getMember(username: string) {
+    // Use the reduce() function to flatten multiple child arrays into one
+    // mutable top-level array - as our cache grows, new member entities will
+    // be added to the end of the array (rather than generating more sub-arrays).
+
+    // Function documentation: Calls the specified callback function for all the
+    // elements in an array. The return value of the callback function is the
+    // accumulated result, and is provided as an argument in the next call to
+    // the callback function.
+
+    // Note that this approach will add "duplicate" entities to our single array,
+    // so the results need be searched with a find() function.
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((member: Member) => member.userName === username);
+
+    if (member) return of(member);
+
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member): Observable<any> {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        // The spread (...) syntax allows an iterable, such as an array or string,
+        // to be expanded in places where zero or more arguments (for function calls)
+        // or elements (for array literals) are expected. In an object literal, the
+        // spread syntax enumerates the properties of an object and adds the key-value
+        // pairs to the object being created.
+        this.members[index] = { ...this.members[index], ...member }
+      })
+    );
+  }
+
+  setMainPhoto(photoId: number): Observable<any> {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number): Observable<any> {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
+
   private getPaginatedResult<T>(url: string, params: HttpParams): Observable<PaginatedResult<T>> {
     const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>;
 
@@ -68,34 +111,5 @@ export class MembersService {
     params = params.append('pageSize', pageSize);
 
     return params;
-  }
-
-  getMember(username: string) {
-    const member = this.members.find(x => x.userName === username);
-    if (member) return of(member);
-
-    return this.http.get<Member>(this.baseUrl + 'users/' + username);
-  }
-
-  updateMember(member: Member): Observable<any> {
-    return this.http.put(this.baseUrl + 'users', member).pipe(
-      map(() => {
-        const index = this.members.indexOf(member);
-        // The spread (...) syntax allows an iterable, such as an array or string,
-        // to be expanded in places where zero or more arguments (for function calls)
-        // or elements (for array literals) are expected. In an object literal, the
-        // spread syntax enumerates the properties of an object and adds the key-value
-        // pairs to the object being created.
-        this.members[index] = { ...this.members[index], ...member }
-      })
-    );
-  }
-
-  setMainPhoto(photoId: number): Observable<any> {
-    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
-  }
-
-  deletePhoto(photoId: number): Observable<any> {
-    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 }
