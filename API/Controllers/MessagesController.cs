9eff707b35216a4edd9e40da3f24cfb0e9468120
Kevin Schanz <kevinschanz@live.com>
2023-09-07 17:38:10 -0400

    Add the Messages Controller API and create some test messages between Lisa and Todd users

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,62 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class MessagesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+
+        public MessagesController(IUserRepository userRepository,
+            IMessageRepository messageRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _messageRepository = messageRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto request)
+        {
+            var username = User.GetUsername();
+
+            if (username == request.RecipientUsername.ToLower())
+            {
+                return BadRequest("You cannot send messages to yourself!");
+            }
+
+            var sender = await _userRepository.GetUserByUsernameAsync(username);
+            var recipient = await _userRepository.GetUserByUsernameAsync(request.RecipientUsername);
+
+            if (recipient == null)
+            {
+                return NotFound();
+            }
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = request.Content
+            };
+
+            _messageRepository.AddMessage(message);
+
+            if (await _messageRepository.SaveAllAsync())
+            {
+                // TODO: Refactor to return a 201 Created.
+                return Ok(_mapper.Map<MessageDto>(message));
+            }
+
+            return BadRequest("Failed to send message");
+        }
+    }
+}
