cce25c37d3e3c051f8ce85233a7d9eb7ddd474e3
Kevin Schanz <kevinschanz@live.com>
2023-09-28 18:16:58 -0400

    Add 'Delete Message' API in the .NET arch

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -42,9 +42,13 @@ namespace API.Data
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username),
-                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username),
-                _ => query.Where(u => u.RecipientUsername == messageParams.Username && u.DateRead == null)
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username
+                    && u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username
+                    && u.SenderDeleted == false),
+                _ => query.Where(u => u.RecipientUsername == messageParams.Username
+                    && u.RecipientDeleted == false
+                    && u.DateRead == null)
             };
 
             // Extension method to project from a queryable using the provided mapping engine.
@@ -64,8 +68,10 @@ namespace API.Data
                 .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(
                     m => m.RecipientUsername == currentUserName
+                        && m.RecipientDeleted == false
                         && m.SenderUsername == recipientUserName
                         || m.RecipientUsername == recipientUserName
+                        && m.SenderDeleted == false
                         && m.SenderUsername == currentUserName)
                 .OrderBy(m => m.MessageSent)
                 .ToListAsync();
